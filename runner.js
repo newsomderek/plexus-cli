c3ef63eae30fe34676a6428e11cb1e1c212548b2
newsomderek <newsom.derek@gmail.com>
2017-07-12 14:15:48 -0700

    Promisfy all the things and improve logic flow. WIP

--- runner.js
+++ runner.js
@@ -1,70 +1,82 @@
-
 let flow;
 
 const fs = require('fs');
+const Promise = require('bluebird');
 
 const PLEX = require('./sys/plex.js');
 const ACTIONS = PLEX.util.compileActions();
 
 // global context value store
-let _  = {};
+let _ = {};
 
 // load any context from file if it exists
-try {    
-    let context = JSON.parse(fs.readFileSync('context.json', 'utf8'));   
-    Object.assign(_, context); 
+try {
+    let context = JSON.parse(fs.readFileSync('context.json', 'utf8'));
+    Object.assign(_, context);
 } catch (err) {
 
 }
 
 // try to load flow details from global context
-try {    
-    if(_.flow) flow = require(_.flow);
+try {
+    if (_.flow) flow = require(_.flow);
 } catch (err) {
 
 }
 
-let run = (nodeId, node) => new Promise((resolve, reject) => {
-    let action = PLEX.util.findActionByNodeId(ACTIONS, nodeId);
+let run = (nodes, flow) => new Promise((resolve, reject) => {
 
-    if(!action) return reject(`${nodeId} is not an available action`);
+    nodes.forEach(node => {
+        let action = PLEX.util.findActionByNodeId(ACTIONS, node.id);
 
-    action(_, node.params).then(res => {
+        if (!action) return reject(`${node.id} is not an available action`);
 
-        // load return into global context
-        _[nodeId] = {result: res};
-
-        // traverse through child action nodes
-        node.edges.forEach(edge => {
-            let next = flow.nodes[edge];
-
-            if(!next) return;
-
-            run(edge, next).then(result => {
+        action(_, node.params)
+            .then(result => {
                 console.log(`RUN RESULT: ${JSON.stringify(result)}`);
-            }).catch(reason => {
-                console.log(`RUN ERROR: ${reason}`);  
-            });
-        });
 
-        return resolve(res);
+                // load return into global context
+                _[node.id] = { result };
+
+                return result;
+            })
+            .then(result => {
+
+                let edgeNodes = node.edges.map(nodeId => {
+                    let composite = { id: nodeId };
+                    Object.assign(composite, flow.nodes[nodeId])
+
+                    return composite;
+                });
+
+                if (node.isLoop) {
+
+                    Promise.map(result, (item) => {
+                        return run(edgeNodes, flow);
+                    });
+
+                } else {
+                    return run(edgeNodes, flow);
+                }
+            });
+
     });
+
+    return resolve({ status: 'success' });
 
 });
 
-if(flow) {
-    
-    let source = {
-        edge: flow.source,
-        node: flow.nodes[flow.source]
-    };
+if (flow) {
 
-    run(source.edge, source.node).then(result => {
+    let source = { id: flow.source };
+    Object.assign(source, flow.nodes[flow.source]);
 
+    run([source], flow).then(result => {
+        console.log(result.status);
     }).catch(reason => {
         console.log(`ERROR: ${reason}`);
     });
-    
+
 } else {
     console.log('No flow found to execute');
 }
